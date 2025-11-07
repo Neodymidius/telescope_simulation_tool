f2759804cc252040ff8df3a08606e9020c2816bb
Neodymidius <neoreinmann@protonmail.com>
2025-11-07 17:09:34 +0100

    refactor(unification): adds Plane into Sensor #7

--- src/sensor/Sensor.h
+++ src/sensor/Sensor.h
@@ -6,14 +6,17 @@ Copyright (C) 2025  Neo Reinmann (neoreinmann@gmail.com)
 #define SIXTE_SENSOR_H
 
 #include "geometry/Ray.h"
+#include "shape/Plane.h"
 
 class Sensor {
 public:
     Sensor();
     Sensor(std::string file_name, const Vec3fa& position);
+    Sensor(Plane_parameters plane_parameters);
     std::string filename;
     Vec3fa position;
     unsigned int geomID = -1;
+    Plane plane_;
 };
 
 
